14364c9a9d154cf518d15f348982b959d7274571
unknown <wsjimenez1@utpl.edu.ec>
2023-02-23 23:58:13 -0500

    cambios 6 en patients mostrar.php

--- vista/user-patients/patiens-mostrar.php
+++ vista/user-patients/patiens-mostrar.php
@@ -59,7 +59,7 @@
                             <span class="ml-4 text-white">Mis Citas</span>
                         </a>
                     </li>
-                    <li class="relative px-6 py-3">
+                    <!--<li class="relative px-6 py-3">
                         <span class="absolute inset-y-0 left-0 w-1  rounded-tr-lg rounded-br-lg"
                             aria-hidden="true"></span>
                         <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
@@ -72,7 +72,7 @@
                             </svg>
                             <span class="ml-4 text-white">Citas Agendadas</span>
                         </a>
-                    </li>
+                    </li>-->
                 </ul>
 
 
