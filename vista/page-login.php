2dc32dbf3284388a9b5f6e51d4d823ec7d2536fa
unknown <wsjimenez1@utpl.edu.ec>
2023-02-24 00:03:54 -0500

    cambios en login y registro

--- vista/page-login.php
+++ vista/page-login.php
@@ -127,7 +127,7 @@ if (isset($_POST['login'])) {
                                 <span class="text-gray-700 dark:text-gray-400">Usuario</span>
                                 <input
                                     class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                    placeholder="Digita tu Usuario" name="usuario"
+                                    placeholder="Digita tu Usuario" name="usuario" required
                                     value="<?php if (isset($_POST['usuario'])) {
                                         echo $_POST['usuario'];
                                     } ?>"
